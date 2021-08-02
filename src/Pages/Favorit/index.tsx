0c85e19923fd71c6dde183012b278a1ff509961e
nawan44 <rachmat.d.gunawan@gmail.com>
2021-08-03 00:00:27 +0700

    get 2

--- src/Pages/Favorit/index.tsx
+++ src/Pages/Favorit/index.tsx
@@ -24,74 +24,57 @@ import { useEffect, useState } from "react";
     image: "",
   });
   console.log("dataContact", dataContact);
-//   const getDataContact = async (e: React.MouseEvent<HTMLElement>) => {
-//        const data = {
-//        name: dataContact?.name,
-//          phone: dataContact?.phone,
-//          job: dataContact?.job,
-//          company: dataContact?.company,
-//          email: dataContact?.email,
-//          image: dataContact?.image,
-//        };
-//   const myHeaders = new Headers({
-//     'Content-Type': 'application/json',
-//     'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImwyMDAxNDAwMDRAZ21haWwuY29tIiwicGFzc3dvcmQiOiIyNjJlZTQwMjcxYzdkZDMyM2EzZWNmNDIwMjg3ZjRhYyIsImlhdCI6MTYwNjcyNzU3MywiZXhwIjoxNjA2ODEzOTczfQ.2jnEN_Uxf9mDnOciEm57T3vAcR2GAXEkuwnLHDpofVI'
-// });
-// return fetch('https://phone-book-api.herokuapp.com/api/v1/contacts', {
-//   method: 'GET',
-//   headers: myHeaders,
-// })
 
-// .then(response => {
-//     if (response.status === 200) {
-//       return response.json();
-//     } else {
-//       throw new Error('Something went wrong on api server!');
-//     }
-//   })
-//   .then(response => {
-//     console.debug(response);
-//   }).catch(error => {
-//     console.error(error);
-//   });
-// }
-
-// getDataContact();
-// }
 useEffect(() => {
   getDataContact()
 }, []);
   const getDataContact = async () => {
-    const data = {
-      name: dataContact?.name,
-      phone: dataContact?.phone,
-      job: dataContact?.job,
-      company: dataContact?.company,
-      email: dataContact?.email,
-      image: dataContact?.image,
-    };
+    // const data = {
+    //   name: dataContact?.name,
+    //   phone: dataContact?.phone,
+    //   job: dataContact?.job,
+    //   company: dataContact?.company,
+    //   email: dataContact?.email,
+    //   image: dataContact?.image,
+    // };
 
-    const requestOptions = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImwyMDAxNDAwMDRAZ21haWwuY29tIiwicGFzc3dvcmQiOiIyNjJlZTQwMjcxYzdkZDMyM2EzZWNmNDIwMjg3ZjRhYyIsImlhdCI6MTYwNjcyNjg0NiwiZXhwIjoxNjA2ODEzMjQ2fQ.IMX8_G0beCWWdySB8ggNznR6y4xtEscPepIFQ5nqgLE",
-        },
-    };
-    try {
-      const response = await fetch(
-        "https://phone-book-api.herokuapp.com/api/v1/contacts",
-        requestOptions
-      );
-      const res = await response.json();
-      setDataContact(res)
-      console.log("============================", res);
-      alert("Berhasil Mendaftar");
-    } catch (error) {
-      alert(error);
-      console.log(error);
-    }
+    // const requestOptions = {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization:
+    //       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImwyMDAxNDAwMDRAZ21haWwuY29tIiwicGFzc3dvcmQiOiIyNjJlZTQwMjcxYzdkZDMyM2EzZWNmNDIwMjg3ZjRhYyIsImlhdCI6MTYwNjcyNjg0NiwiZXhwIjoxNjA2ODEzMjQ2fQ.IMX8_G0beCWWdySB8ggNznR6y4xtEscPepIFQ5nqgLE",
+    //     },
+    // };
+    // try {
+    //   const response = await fetch(
+    //     "https://phone-book-api.herokuapp.com/api/v1/contacts",
+    //     requestOptions
+    //   );
+    //   const res = await response.json();
+    //   setDataContact(res)
+    //   console.log("============================", res);
+    //   alert("Berhasil Mendaftar");
+    // } catch (error) {
+    //   alert(error);
+    //   console.log(error);
+    // }
+    var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImwyMDAxNDAwMDRAZ21haWwuY29tIiwicGFzc3dvcmQiOiIyNjJlZTQwMjcxYzdkZDMyM2EzZWNmNDIwMjg3ZjRhYyIsImlhdCI6MTYwNjcyNjg0NiwiZXhwIjoxNjA2ODEzMjQ2fQ.IMX8_G0beCWWdySB8ggNznR6y4xtEscPepIFQ5nqgLE");
+
+var formdata = new FormData();
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  body: formdata,
+  redirect: 'follow'
+};
+
+fetch("https://phone-book-api.herokuapp.com/api/v1/contacts", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
   };
     return (
         <div style={{ backgroundColor: "#EAE7E7" , height:"100vh"}}>
